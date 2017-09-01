5045779ed6ee53dd48c216920d4e30823ef563d8
Kitaev <klim.kitaev@gmail.com>
2017-09-01 17:55:55 +0300

    inventory-app start

---
+++ inventory-app/src/app/product-row/product-row.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input,HostBinding } from '@angular/core';
+import { Product } from '../product.model';
+
+
+@Component({
+  selector: 'app-product-row',
+  templateUrl: './product-row.component.html'
+})
+export class ProductRowComponent {
+
+  @Input() product:Product;
+  @HostBinding('attr.class') cssClass = 'item';
+
+}
